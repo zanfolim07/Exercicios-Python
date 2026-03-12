bb6cdef485abdcf1b0cb77c8730c8927950f8442
zanfolim07 <guilhermezanfolimm@gmail.com>
2026-03-12 20:38:33 -0300

    Adicionar exercicios de algoritimo

---
+++ potencia.py
@@ -0,0 +1,12 @@
+num1 = int(input("digite seu numero:"))
+num2 = int(input("digite seu numero:"))
+
+resultado = num1 + num2 
+print("sua conta deu:", resultado)
+
+x = int(input("Digite seu numero"))
+y = int(input("Digite seu numero"))
+
+resultado = x ** y 
+
+print(resultado)
