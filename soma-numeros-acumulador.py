58308d18799ab0e7bbb124091346082b56deab88
zanfolim07 <guilhermezanfolimm@gmail.com>
2026-03-17 21:57:09 -0300

    Reorganiza estrutura do repositório

---
+++ soma-numeros-acumulador.py
@@ -0,0 +1,18 @@
+soma = 0
+valor = input("Digite um numero:")
+num = int(valor)
+soma = soma + num 
+
+valor = input("Digite um numero:")
+num = int(valor)
+soma = soma + num 
+
+valor = input("Digite um numero:")
+num = int(valor)
+soma = soma + num 
+
+valor = input("Digite um numero:")
+num = int(valor)
+soma = soma + num 
+
+print (soma)
